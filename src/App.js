9469322f9a877e31cd130bfff832b557da9a78cc
devagx <alungroome@hotmail.com>
2021-02-14 14:27:27 +0000

    Added styles to App.css and index.css

--- src/App.js
+++ src/App.js
@@ -4,20 +4,50 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header class="header">
+        <h1 class="heading">To Do!!</h1>
+        <div class="add-task__actions">
+          <label for="add-task-input" class="visuallyhidden">Add new to do task</label>
+          <input id="add-task-input" class="add-task__input-field"></input>
+          <button class="button">Add</button>
+        </div>
+        <p class="outstanding-tasks">You have 3 tasks to complete</p>
       </header>
+      <main class="all-tasks">
+        <section class="incomplete-tasks">
+          <h2 class="heading">Tasks to do:</h2>
+          <ul class="task-list">
+            <li class="task">
+              <p class="task__text">This is a task that needs to be done</p>
+              <button class="button">Complete</button>
+              <button class="button">Delete</button>
+            </li>
+            <li class="task">
+              <p class="task__text">This is a task that needs to be done</p>
+              <button class="button">Complete</button>
+              <button class="button">Delete</button>
+            </li>
+            <li class="task">
+              <p class="task__text">This is a task that needs to be done</p>
+              <button class="button">Complete</button>
+              <button class="button">Delete</button>
+            </li>
+          </ul>
+        </section>
+        <section class="complete-tasks">
+          <h2 class="heading">Complete tasks:</h2>
+          <ul class="task-list">
+            <li class="task task--complete">
+              <p class="task__text">This is a task that needs to be done</p>
+              <button class="button">Delete</button>
+            </li>
+            <li class="task task--complete">
+              <p class="task__text">This is a task that needs to be done</p>
+              <button class="button">Delete</button>
+            </li>
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
