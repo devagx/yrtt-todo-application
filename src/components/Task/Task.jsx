9d4c27e6abc92cfa01b9aabba4df216d7debd216
devagx <alungroome@hotmail.com>
2021-02-19 22:00:27 +0000

    refactor of deleteTask

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,14 +1,10 @@
 import "./Task.css";
 
-function Task({ complete, text, id, deleteTask }) {
-  const handleClick = () => {
-    deleteTask(id);
-  };
   return (
     <li className={`task ${complete ? "task--complete" : ""}`}>
       <p className="task__text">{text}</p>
       {complete === false && <button className="button">Complete</button>}
-      <button onClick={handleClick} className="button">
+      <button onClick={() => deleteTask(id)} className="button">
         Delete
       </button>
     </li>
